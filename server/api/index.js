34822e9e02d3a58d04a5a5c8fe9bfb6bb16d3860
Fanny Jiang <jiangf@bu.edu>
2017-08-11 18:24:09 -0400

    implements oauth

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/users', require('./users'))
+
 router.use((req, res, next) => {
   const err = new Error('Not found.')
   err.status = 404

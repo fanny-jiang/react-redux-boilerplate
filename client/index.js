245bab3b30c51580f857c73f8b5634d3025494eb
Fanny Jiang <jiangf@bu.edu>
2017-08-11 15:56:46 -0400

    set up react

--- client/index.js
+++ client/index.js
@@ -1,30 +1,7 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const bodyParser = require('body-parser')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const app = express()
-
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './public')))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use('/api', require('../server/api'))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-})
-
-app.use((err, req, res, next) => {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`Your server, listening on port ${port}`)
-})
+ReactDOM.render(
+  <div>Hello world!</div>,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+)
